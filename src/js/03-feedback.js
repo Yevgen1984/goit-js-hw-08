60442a9ef77171c69680ec6f5db5b9711702f74e
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-09-02 18:37:10 +0300

    HW 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import * as throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const SAVED_KEY = 'feedback-form-state';
+const formValue = {};
+
+form.addEventListener('input', throttle(onGetData, 500));
+form.addEventListener('submit', sendForm);
+
+function onGetData(event) {
+  formValue[event.target.name] = event.target.value; /* запитати у Руслана */
+
+  localStorage.setItem(SAVED_KEY, JSON.stringify(formValue));
+}
+
+function saveMessage() {
+  const obj = JSON.parse(localStorage.getItem(SAVED_KEY));
+
+  const textarea = document.querySelector('textarea');
+  const input = document.querySelector('input');
+
+  if (localStorage.getItem(SAVED_KEY)) {
+    textarea.value = obj.message;
+    input.value = obj.email;
+  }
+}
+saveMessage();
+
+function sendForm(event) {
+  event.preventDefault();
+  console.log(formValue);
+  event.target.reset(); /* запитати у Руслана про currentTarget/target */
+  localStorage.removeItem(SAVED_KEY);
+}
